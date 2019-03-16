bac9bb84a5762a8bc2236cd6d398841096af2142
Yoidea <yoidea.cc@gmail.com>
2019-03-17 03:53:13 +0900

    Add animista.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,20 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import "./animista.css"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <section className="hero is-success is-fullheight">
+    <section className="hero is-success is-fullheight"
+      style={{backgroundColor: "#009688"}}
+    >
       <div className="hero-body">
-        <div className="container">
-          <h1 className="title">
+        <div className="container has-text-centered">
+          <h1 className="title is-size-1 focus-in-contract-bck">
             入江一帆（ラムダ）
           </h1>
-          <h2 className="subtitle">
+          <h2 className="subtitle focus-in-contract-bck-1s">
             学生 / エンジニア志望 / 塾講師
           </h2>
         </div>
