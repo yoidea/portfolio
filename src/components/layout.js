80dc67b2e2fe1ae62012b89e71cc7231b65c476a
yoidea <yoidea.cc@gmail.com>
2019-03-16 19:24:47 +0900

    Add bulma

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import "bulma"
 import "./layout.css"
 
 const Layout = ({ children }) => (
