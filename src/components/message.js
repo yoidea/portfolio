25862bbdbc9584f3c3e1af0fb96dff8bc022d1d7
yoidea <yoidea.cc@gmail.com>
2019-03-22 02:15:40 +0900

    Make a Message component

---
+++ src/components/message.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react"
+
+class Message extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      display: true
+    }
+  }
+
+  render() {
+    return this.state.display ? (
+      <div className="container" style={{
+        position: "fixed",
+        top: "3rem",
+        left: "0",
+        right: "0",
+        padding: "1rem"
+      }}>
+        <div className={`notification has-text-centered ${this.props.className}`}>
+          <button className="delete" onClick={() => {this.setState({ display: false })}}></button>
+          <p className="title">{this.props.children}</p>
+        </div>
+      </div>
+    ) : ""
+  }
+}
+
+export default Message
