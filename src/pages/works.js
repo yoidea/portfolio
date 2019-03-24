3bab8af3db02f1b5f62f2505c7df630bbd1292af
yoidea <yoidea.cc@gmail.com>
2019-03-24 17:57:21 +0900

    Add contents to works page

--- src/pages/works.js
+++ src/pages/works.js
@@ -7,6 +7,7 @@ import SEO from "../components/seo"
 import { Heading } from "../components/typography"
 
 import "./animista.css"
+import smat from "../images/smat_thumbnail.png"
 
 class WorksPage extends Component {
   constructor(props) {
@@ -18,7 +19,25 @@ class WorksPage extends Component {
       <Layout>
         <SEO title="Works" keywords={[`gatsby`, `application`, `react`]} />
         <Hero color="#e91e63">
-          <Heading>作ったもの</Heading>          
+          <Heading>作ったもの</Heading>
+          <div class="columns is-desktop">
+            <div class="column is-4">
+              <figure class="image is-4by3">
+                <img src={smat} />
+              </figure>
+            </div>
+            <div class="column">
+              <h1 className="title">Smart Teach</h1>
+              <p>小テストの作成から出題、採点、分析までをサポートするアプリケーション</p>
+              <p>チーム開発で主にフロントエンドを担当した。</p>
+              <div class="tags are-medium">
+                <span class="tag is-dark">React</span>
+                <span class="tag is-dark">Rails</span>
+                <span class="tag is-dark">Heroku</span>
+              </div>
+              <p><a target="_blank" href="https://smat-dev.herokuapp.com/" className="button is-danger is-inverted is-outlined">ページを見る</a></p>
+            </div>
+          </div>
         </Hero>
       </Layout>
     )
