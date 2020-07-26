df5eddd5f2fd72007f054d17d08bfecc943be686
Yoidea <yoidea.cc@gmail.com>
2020-07-26 20:39:44 +0900

    feat: 🎸 Add videos page

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -6,6 +6,9 @@ import SEO from "../components/seo";
 import { Heading } from "../components/typography";
 
 import "./animista.css";
+import thumbnail1 from "../images/yt_thumbnail1.png";
+import thumbnail2 from "../images/yt_thumbnail2.png";
+import thumbnail3 from "../images/yt_thumbnail3.png";
 
 class VideosPage extends Component {
   render() {
@@ -14,15 +17,56 @@ class VideosPage extends Component {
         <SEO title="Movies" keywords={[`gatsby`, `application`, `react`]} />
         <Hero color="#ff9800">
           <Heading>動画</Heading>
+          <p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.youtube.com/channel/UC8g17oXkRt8buwQL8GU-NSw"
+            >
+              ラムダ技術部 / Yoidea
+            </a>
+            というチャンネルで技術系の真面目な動画や不真面目な動画を投稿しています。
+          </p>
+          <p>不真面目な動画の方が伸びるので多めかもしれません。</p>
+          <p>
+            プログラミングや電子工学、数学などに興味を持つきっかけになれると嬉しいです。
+          </p>
           <div class="columns is-desktop">
             <div class="column">
-              <h1 className="title">クソダサいスライドを作ろう</h1>
+              <h1 className="title">【騙されないで】画面の捏造ちょろ過ぎて草</h1>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.youtube.com/watch?v=GXZZB5vO78c"
+              >
+                <figure class="image is-16by9">
+                  <img src={thumbnail1} alt="Smart Teach" />
+                </figure>
+               </a>
             </div>
             <div class="column">
-              <h1 className="title">ワンクリック詐欺を作ろう</h1>
+              <h1 className="title">【決定版】オンライン授業をサボる方法</h1>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.youtube.com/watch?v=YM12QR1m1O8"
+              >
+                <figure class="image is-16by9">
+                  <img src={thumbnail2} alt="Smart Teach" />
+                </figure>
+              </a>
             </div>
             <div class="column">
-              <h1 className="title">うるさい音声を作ろう</h1>
+              <h1 className="title">【理系】超精密なフレンチトーストを作ろう</h1>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.youtube.com/watch?v=G476yr41Jfw"
+              >
+                <figure class="image is-16by9">
+                  <img src={thumbnail3} alt="Smart Teach" />
+                </figure>
+              </a>
             </div>
           </div>
         </Hero>
