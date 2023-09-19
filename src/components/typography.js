d5d55844109af5425ae25fe790fd0d0b2b18aea6
yoidea <yoidea.cc@gmail.com>
2023-09-20 06:03:25 +0900

    PGP公開鍵を追加

--- src/components/typography.js
+++ src/components/typography.js
@@ -11,3 +11,24 @@ export const Heading = ({ children }) => (
     {children}
   </h1>
 );
+
+export const CodeBlock = ({ children }) => (
+  <div className="columns is-mobile is-centered">
+    <pre
+      className="has-text-left is-size-3-fullhd is-size-4-desktop is-size-6"
+      style={{
+        color: "white",
+        background: "linear-gradient(135deg,#ff1744,#ff9100,#76ff03,#009944,#1de9b6,#3d5afe,#d500f9)",
+        WebkitBackgroundClip: "text",
+        WebkitTextFillColor: "transparent",
+        border: "2px solid",
+        borderImage: "linear-gradient(135deg,#ff1744,#ff9100,#76ff03,#009944,#1de9b6,#3d5afe,#d500f9)",
+        borderImageSlice: "1",
+        borderRadius: "1em",
+        marginTop: "1em"
+      }}
+    >
+      {children}
+    </pre>
+  </div>
+);
